9129d09989d031765ed6f62b14a3805fab5d9167
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-08-02 20:57:06 +0430

    Initial Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const Colors = require('./lib/colors');
+const Styles = require('./lib/styles');
+const convert = require('./lib/convert');
+
+module.exports.colors = Colors;
+
+module.exports.styles = Styles;
+
+module.exports.console = {
+  success: (message, styles = {}) => {
+    console.log(convert(message, Colors.Green, styles));
+  },
+  successBG: (message, styles = {}) => {
+    console.log(convert(message, Colors.BgGreen, styles));
+  },
+  info: (message, styles = {}) => {
+    console.log(convert(message, Colors.Cyan, styles));
+  },
+  infoBG: (message, styles = {}) => {
+    console.log(convert(message, Colors.BgCyan, styles));
+  },
+  warning: (message, styles = {}) => {
+    console.log(convert(message, Colors.Yellow, styles));
+  },
+  warningBG: (message, styles = {}) => {
+    console.log(convert(message, Colors.BgYellow, styles));
+  },
+  error: (message, styles = {}) => {
+    console.log(convert(message, Colors.Red, styles));
+  },
+  errorBG: (message, styles = {}) => {
+    console.log(convert(message, Colors.BgRed, styles));
+  },
+  custom: (message, color = Colors.White, styles = {}) => {
+    console.log(convert(message, color, styles));
+  },
+};
